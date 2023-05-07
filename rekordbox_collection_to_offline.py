3a51cb8fc8294221e77b6780f139b59af92b16fd
Yuval <yuvalsegev66@gmail.com>
2023-05-07 20:48:12 +0300

    Seems to work fine

--- rekordbox_collection_to_offline.py
+++ rekordbox_collection_to_offline.py
@@ -4,53 +4,17 @@ import yaml
 import argparse
 import os
 import logging
+import sys
+from urllib.parse import unquote
+import xml.etree.ElementTree as ET
+import subprocess
 
 parser = argparse.ArgumentParser(
                     prog='Convert rekordbox to tidal')
-parser.add_argument('-f', '--file')      # option that takes a value
+parser.add_argument('-f', '--file')
 args = parser.parse_args()
 
-# login_details = {"token_type": None, "access_token": None, "refresh_token": None, "expiry_time": None}
-
-# try:
-#     with open(r'C:\Users\yuval\tidal_creds.yaml') as file:
-#         login_details_temp = yaml.full_load(file)
-#         if(login_details_temp):
-#             login_details = login_details_temp
-
-# except:
-#     pass
-
-# session = tidalapi.Session()
-
-# if login_details["access_token"]:
-#     session.load_oauth_session(login_details["token_type"], login_details["access_token"], login_details["refresh_token"], datetime.datetime.fromtimestamp(login_details["expiry_time"]))
-# else:
-#     # Will run until you visit the printed url and link your account
-#     session.login_oauth_simple()
-
-
-
-# login_details['token_type'] = session.token_type
-# login_details['access_token'] = session.access_token
-# login_details['refresh_token'] = session.refresh_token # Not needed if you don't care about refreshing
-# login_details['expiry_time'] = session.expiry_time.timestamp()
-
-# print(login_details)
-# with open(r'C:\Users\yuval\tidal_creds.yaml', 'w') as file:
-#     documents = yaml.dump(login_details, file)
-
-tidal_tracks = []
-
-# for line in open(args.file, "rb"):
-#     print(line)
-
-import xml.etree.ElementTree as ET
-
-# down_path = "C:/users/yuval/Music/tidal/"
 down_path =  os.path.join(os.path.expanduser("~"), "Music\\tidal\\")
-print(down_path)
-import subprocess
 
 def download_track(track_id):
     try:
@@ -71,15 +35,57 @@ root = tree.getroot()
 
 tracks = root.findall("COLLECTION")[0].findall("TRACK")
 
+def find_duplicates(new_track, track_path):
+    duplicates = []
+    # Search for tracks with the same file path in the collection
+    for track in tracks:
+        try:
+            if os.path.samefile(unquote(track.get("Location").replace("file://localhost/", "")), track_path):
+                print("found duplicate", track, new_track)
+                duplicates.append(track)
+        except:
+            pass
+
+    output_track = new_track
+    num_children = len({t for t in new_track})
+
+    # Find the duplicate track with the most children (hotcues, beatgrid adjustments)
+    for duplicate in duplicates:
+        if len({t for t in duplicate}) > num_children:
+            num_children = len({t for t in duplicate})
+            output_track = duplicate
+
+    return (duplicates, output_track)
+
+def merge_tracks(duplicates, target):
+    # Delete all of the unrelevatnt
+    # for playlist in root.findall("PLAYLISTS")[0].findall("NODE").iter("NODE"):
+    relevant_ids = {duplicate.get("TrackID") for duplicate in duplicates}
+    target_id = target.get("TrackID")
+
+    print(relevant_ids, "target", target_id)
+    for duplicate in duplicates:
+        if duplicate != target:
+            root.findall("COLLECTION")[0].remove(duplicate)
+
+    for track in root.findall("PLAYLISTS")[0].iter("TRACK"):
+        # print(track.get("Key"))
+        if track.get("Key") in relevant_ids:
+            print("relevant", track.get("Key"))
+            track.set("Key", target_id)
+
+
+# Iterate over all tracks, download TIDAL ones and merge duplicates
+# Duplciates will happen when importing an offline TIDAL track the collection. 
 for track in root.findall("COLLECTION")[0].iter("TRACK"):
-    # track = tracks[i]
-    print(track.get("Location"))
     if track.get("Location").startswith("file://localhost/tidal:tracks:"):
         track_id = track.get("Location").split(":", 3)[3]
         print(track.get("Name"))
         track_path = download_track(int(track_id))
         if track_path:
             track.set("Location", "file://localhost/" + track_path)
-        # print(track.get("Location"))
+            duplicates, target_track = find_duplicates(track, track_path)
+            merge_tracks(duplicates, target_track)
+
 
 tree.write("collection_offline.xml", encoding="utf-8", xml_declaration=True)
